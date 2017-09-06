7856b6c871ccb9f2f281f03262fc0d7f8ee0e19f
alexanderrichard <alex-richard@web.de>
2017-09-06 14:26:52 +0200

    Update FE_helper.py

--- FE_helper.py
+++ FE_helper.py
@@ -4,6 +4,7 @@ import extractor
 path = '/media/datasets/action_recognition/kinetics/validation_new/'
 clss = [osp.join(path, cl) for cl in os.listdir(path) if osp.isdir(osp.join(path, cl))]
 
+# no matter what you do here, make sure at the end vids contains a list with all video filenames
 vids = []
 for cl in clss:
 	vids += [osp.join(cl, vid) for vid in os.listdir(cl) if osp.isfile(osp.join(cl, vid)) and vid.endswith('.avi')]
